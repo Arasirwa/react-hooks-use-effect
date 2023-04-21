e0848bdbd8961b0f92cb90fcba13a0276b9c049e
william arasirwa <mageboarasirwa03@gmail.com>
2023-04-21 16:44:23 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { useState, useEffect } from "react";
-import DogPics from "./DogPics";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    document.title = text;
-  }, [text]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     setTimeout(() => setCount(0), 5000);
   }, []);
 
-  console.log("Component rendering");
+  console.log("rendering");
 
   return (
     <div>
+      <h1>useEffect</h1>
       <button onClick={() => setCount((count) => count + 1)}>
         I've been clicked {count} times
       </button>
       <input
         type="text"
         placeholder="Type away..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <DogPics />
     </div>
   );
 }
